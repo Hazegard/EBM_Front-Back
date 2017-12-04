efcbcd7b4b854ba91c7cfad24cbf92e97533409b
Maxime Catrice <m.catrice@outlook.com>
2017-12-04 16:22:09 +0100

    add working onclick function

--- Front/src/js/api_requests.js
+++ Front/src/js/api_requests.js
@@ -1,10 +1,13 @@
 function getArticles() {
     $.getJSON('/api/v1/articles/').done(function (data) {
-        let titles = [];
         $(".dropdown-item").remove();
         for (let i in data) {
-            titles.push(data[i].TITLE);
-            $(".dropdown-menu").append('<a class="dropdown-item">' + titles[i] + '</a>');
+            let item = $('<a class="dropdown-item">' + data[i].TITLE + '</a>').on("click", function () {
+                console.log($(this).data('article'))
+            })
+                .data("article", data[i]);
+            $(".dropdown-menu").append(item)
+
         }
     })
 }
