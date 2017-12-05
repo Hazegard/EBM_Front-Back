2694b447113640b6ca7642606a80a0a409078951
Ekar <antoine.robillard@noos.fr>
2017-12-05 15:56:00 +0100

    populate database and prettify paragraphs

--- Front/src/js/api_requests.js
+++ Front/src/js/api_requests.js
@@ -1,10 +1,10 @@
 getArticles = () => {
     $.getJSON('/api/v1/articles/').done(function (data) {
         $(".dropdown-item").remove();
+        console.log(data); // TODO : À enlever plus tard
         for (let i in data) {
             let item = $('<a class="dropdown-item">' + data[i].TITLE + '</a>')
                 .on("click", function () {
-                    console.log($(this).data('article')); // TODO: À enlever quand on aura fini
                     displayParagraphs($(this).data('article').TITLE, $(this).data('article').CONTENT);
                 })
                 .data("article", data[i]);
@@ -13,8 +13,13 @@ getArticles = () => {
     })
 };
 
+emptyParagraphs = () => {
+    let item = $('<p>Sélectionnez un article...</p>');
+    $('#paragraphs').empty().append(item);
+};
+
 displayParagraphs = (title, paragraphs) => {
-    let item = $('<div class="container"><h1 class="display-3">' + title + '</h1><hr class="my-2">');
-    paragraphs.map(para => item.append('<p class="lead">' + para.CONTENT + '</p>'));
+    let item = $('<div class="container"><h1 class="display-4">' + title + '</h1><hr class="my-2">');
+    paragraphs.map(para => item.append('<p class="lead text-justify">' + para.CONTENT + '</p>'));
     $('#paragraphs').empty().append(item);
 };
