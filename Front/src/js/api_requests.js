1c2d81ff04f223e383a49e11f2dcf0068ed7c898
Ekar <antoine.robillard@noos.fr>
2017-12-05 13:43:31 +0100

    add show articles feature

--- Front/src/js/api_requests.js
+++ Front/src/js/api_requests.js
@@ -4,10 +4,17 @@ function getArticles() {
         for (let i in data) {
             let item = $('<a class="dropdown-item">' + data[i].TITLE + '</a>')
                 .on("click", function () {
-                    console.log($(this).data('article'))
+                    console.log($(this).data('article')); // TODO: À enlever quand on aura fini
+                    displayParagraphs($(this).data('article').TITLE, $(this).data('article').CONTENT);
                 })
                 .data("article", data[i]);
             $("#listeArticles").append(item)
         }
     })
+}
+
+function displayParagraphs(title, paragraphs) {
+    let item = $('<div class="container"><h1 class="display-3">' + title + '</h1>');
+    paragraphs.map(para => item.append('<p class="lead">' + para.CONTENT + '</p>'));
+    $('#paragraphs').empty().append(item);
 }
