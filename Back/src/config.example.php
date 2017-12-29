49f813582d86650a6dd2ebe279b34e31d43849d1
Maxime Catrice <m.catrice@outlook.com>
2017-12-29 22:39:35 +0100

    add config.example.php

---
+++ Back/src/config.example.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maxime
+ * Date: 22/11/17
+ * Time: 21:26
+ */
+
+$host = '172.17.0.2';
+$dbname = 'Tp_Final';
+$username = 'root';
+$password = 'mysql';
+
+$headerPass = '8f5a91bbc3f768a227d4c67022197bd173084d6af578729e283ea8b7b51e5169';
+// pwd: 159632_SQl
+// usr: Front_Back
