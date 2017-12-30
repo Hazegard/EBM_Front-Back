4a5cc80b24cb1c1ce661feb3fbe6d3b78fbbd7ab
Maxime Catrice <m.catrice@outlook.com>
2017-12-30 14:52:57 +0100

    update config.example.php

--- Back/src/config.example.php
+++ Back/src/config.example.php
@@ -6,11 +6,7 @@
  * Time: 21:26
  */
 
-$host = '172.17.0.2';
-$dbname = 'Tp_Final';
-$username = 'root';
-$password = 'mysql';
-
-$headerPass = '8f5a91bbc3f768a227d4c67022197bd173084d6af578729e283ea8b7b51e5169';
-// pwd: 159632_SQl
-// usr: Front_Back
+$host = '';
+$dbname = '';
+$username = '';
+$password = '';
